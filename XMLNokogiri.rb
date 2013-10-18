0e68abfd3579ffb7cced54b203ab40dc4c6dccf3
geeknbar <dorian.coffinet@gmail.com>
2013-10-18 16:11:38 +0200

    install gem nokogiri and definied main class(doesn't work), run XMLNokogiri to show results

--- XMLNokogiri.rb
+++ XMLNokogiri.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require 'nokogiri'
  
 class XMLNokogiri
@@ -86,8 +87,42 @@ class XMLNokogiri
 	  end
 	 
 	  myFile = File.open(myfileName, 'w')
-	  @myDoc.write_xml_to(myFile, :indent =&gt; 4, :encoding =&gt; 'UTF-8')
+	  @myDoc.write_xml_to(myFile, :indent => 4, :encoding => 'UTF-8')
 	  myFile.close
 	end
 
 end
+
+# Load the XML doc
+myXMLNokogiri = XMLNokogiri.new("./test.xml")
+# Print the XML doc
+myXMLNokogiri.readXML()
+# Get the node of the gecko named Green
+myXMLNokogiri.searchNode('//gecko[@name = "Green"]')
+ 
+# Add a new gecko node with an attribute name Color
+myNewNode = myXMLNokogiri.createANewNode('gecko')
+myXMLNokogiri.addAnAttribute(myNewNode, 'name', 'Color')
+myXMLNokogiri.addARootNode(myNewNode)
+ 
+# Add a node couleur to the node we have just added
+myNewNode = myXMLNokogiri.createANewNode('couleur')
+myXMLNokogiri.addText(myNewNode, 'Multicolor')
+myNode = myXMLNokogiri.searchNode('//gecko[@name = "Color"]')
+myXMLNokogiri.insertAChildNode(myNode, myNewNode)
+ 
+# We create a new doc and save it at the place of the old one (yeah, it's just to test eh :p)
+myXMLNokogiri.myDoc = myXMLNokogiri.createANewDoc()
+ 
+# We create a new node with an attribute
+myNewNode = myXMLNokogiri.createANewNode('gecko')
+myXMLNokogiri.addAnAttribute(myNewNode, 'name', 'Geckogeek')
+# We create a new node and add it to the gecko node we have created
+myNewChildNode = myXMLNokogiri.createANewNode('espece')
+myXMLNokogiri.addText(myNewChildNode, 'Geek')
+myXMLNokogiri.insertAChildNode(myNewNode, myNewChildNode)
+# We add the gecko node to our new tree
+myXMLNokogiri.addARootNode(myNewNode)
+ 
+# We save this tree in a file
+myXMLNokogiri.saveToFile("./test.xml")
